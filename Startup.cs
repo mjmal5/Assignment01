9593e136a458d26e1b794c336cefffd3b97c09a5
mjmal5 <mjmal5@student.monash.edu>
2019-09-08 22:10:10 +1000

    First Commit
    Create the ability to make user roles and assign them to users

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_Assignment01.Startup))]
+namespace FIT5032_Assignment01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
