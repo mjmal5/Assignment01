a7ff39abcf74c124dfb18a923bc847246b38ee2e
mjmal5 <mjmal5@student.monash.edu>
2019-10-27 01:37:56 +1100

    Refined bulk email script

--- Utils/EmailSender.cs
+++ Utils/EmailSender.cs
@@ -12,11 +12,12 @@ using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace FIT5032_Assignment.Utils
 {
+
+    //EMAIL SENDER
     public class EmailSender
     {
         private const String API_KEY = "INSERT API KEY";
-
-        string Path = HttpContext.Current.Server.MapPath("\\Uploads\\Attachments\\");
+        readonly string Path = HttpContext.Current.Server.MapPath("\\Uploads\\Attachments\\");
 
         public void Send(String FromEmail, String subject, String contents, String attachment)
         {
@@ -41,6 +42,7 @@ namespace FIT5032_Assignment.Utils
 
     }
 
+    //BULK EMAIL SENDER
     public class BulkEmailSender
     {
         private const String API_KEY = "INSERT API KEY";
@@ -51,37 +53,40 @@ namespace FIT5032_Assignment.Utils
 
         public void BulkSend(String subject, String contents, String attachment)
         {
+            var client = new SendGridClient(API_KEY);
+            
+            var from = new EmailAddress("news@mariospizza.com", subject);
+            var to = new List<EmailAddress>();
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
 
-            var context = new IdentityDbContext();
-            var users = context.Users.ToList();
-            String ToEmail = "";
+            //Get list of Emails from aspnetusers table 
+            var emailList = new IdentityDbContext();
+            var users = emailList.Users.ToList();
 
+
+            //Populate to list
             for (int i = 0; i < users.Count; i++)
             {
-                ToEmail = users[i].Email;
-
-                var client = new SendGridClient(API_KEY);
-                var from = new EmailAddress("news@mariospizza.com", "A message Mario's Pizza");
-                var to = new EmailAddress(ToEmail, "");
-                var plainTextContent = contents;
-                var htmlContent = "<p>" + contents + "</p>";
-                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-
-                if (attachment != "")
-                {
-                    var bytes = File.ReadAllBytes(Path + attachment);
-                    var file = Convert.ToBase64String(bytes);
-                    msg.AddAttachment(attachment, file);
-                }
-
-                var response = client.SendEmailAsync(msg);
+                var CustomerEmail = users[i].Email;
+                var CustomerName = users[i].UserName;
+                to.Add(new EmailAddress(CustomerEmail, CustomerName));              
             }
 
+            //Create message
+            var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
+
+            //Attach file if applicable
+            if (attachment != "")
+            {
+                var bytes = File.ReadAllBytes(Path + attachment);
+                var file = Convert.ToBase64String(bytes);
+                msg.AddAttachment(attachment, file);
+            }
+
+            var response = client.SendEmailAsync(msg);
 
 
-
-
-            
         }
 
     }
